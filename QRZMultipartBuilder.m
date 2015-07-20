84c01c63a672879479121bec9440d4952fed206e
Ruslan <r.nikolaev@smartweb.com.ua>
2015-07-20 18:27:08 +0300

    Fixed value conversion and wrong markup for first boundary

--- QRZMultipartBuilder.m
+++ QRZMultipartBuilder.m
@@ -22,15 +22,15 @@
 -(NSData *) buildHTTPBody
 {
     NSMutableData *body = [[NSMutableData alloc] init];
-    
+
     // Starting boundary
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",_boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
+    [body appendData:[[NSString stringWithFormat:@"--%@\r\n",_boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+
     // Params
     for (int i=0;i<self.attributes.allKeys.count;i++)
     {
         NSString *key = [self.attributes.allKeys objectAtIndex:i];
-        id value = [self.attributes[key] stringValue];
+        id value = self.attributes[key];
         
         if (key && value)
         {
@@ -39,22 +39,27 @@
                 // Text
                 [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n",key] dataUsingEncoding:NSUTF8StringEncoding]];
                 [body appendData:[[NSString stringWithFormat:@"%@",value] dataUsingEncoding:NSUTF8StringEncoding]];
-                [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", _boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+                if (i != self.attributes.allKeys.count - 1)
+                {
+                    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", _boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+                }
             }
             else
             if ([value isKindOfClass:[UIImage class]])
             {
                 // Image
-                [body appendData:[@"Content-Disposition: form-data; name=\"userfile\"; filename=\"image.jpg\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-                [body appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-                [body appendData:UIImageJPEGRepresentation(value,1.0)];
+                [body appendData:[@"Content-Disposition: form-data; name=\"userfile\"; filename=\"image.png\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+                [body appendData:[@"Content-Type: image/png\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+                [body appendData:UIImagePNGRepresentation(value)];
+                //[body appendData:[@"image" dataUsingEncoding:NSUTF8StringEncoding]];
+
             }
         }
     }
 
     // Ending boundary
     [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",_boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
+
     return body;
 }
 
